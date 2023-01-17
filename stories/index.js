eb0b1667f74119156b1f8dfd9b258292170d90da
KishReboot <russellsalopek@pm.me>
2023-01-17 11:12:16 -0500

    Create appointment form, and create stories for 'create' and 'edit

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -164,10 +165,27 @@ storiesOf("Appointment", module)
     <Status 
       message="Deleting"
     />))
-  .add('Error', () => (
+  .add("Error", () => (
       <Error 
         message="Could not delete appointment."
         onClose={action('onClose')}
       />
     ))
+  .add("Form - create", () => (
+    <Form 
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+    ))
+  .add("Form - edit", () => (
+    <Form
+      name="Russ"
+      interviewers={interviewers}
+      value={2}
+      onSave={action('onSave')}
+      oonCancel={action('onCancel')}
+    />
+  ));
+
     
