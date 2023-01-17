dacb63ae8e8f1de7ff23c2720be9e84441e60db7
KishReboot <russellsalopek@pm.me>
2023-01-17 08:07:37 -0500

    Create formatSpots function that checks for availability of spots, tests pass

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,8 +3,28 @@ import classNames from "classnames";
 
 import "components/DayListItem.scss";
 
+const formatSpots = spots => {
+
+  if (!spots) {
+
+    return 'no spots remaining';
+
+  }
+
+  if (spots === 1) {
+
+    return `${spots} spot remaining`
+
+  }
+
+  return `${spots} spots remaining`
+
+}
+
 export default function DayListItem(props) {
   
+  const spotsAvaiable = formatSpots(props.spots);
+
   let dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': !props.spots,
@@ -15,7 +35,7 @@ export default function DayListItem(props) {
       className={dayClass}
       onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{spotsAvaiable}</h3>
     </li>
   );
 }
