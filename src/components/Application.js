de70b61bc7107d70cf4945d5c4948ba2e27f0727
KishReboot <russellsalopek@pm.me>
2023-01-17 18:25:33 -0500

    Add a / to second axios.get, hopefully fix rendering error

--- src/components/Application.js
+++ src/components/Application.js
@@ -20,7 +20,7 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       Promise.resolve(axios.get('/api/days')),
-      Promise.resolve(axios.get('api/appointments'))])
+      Promise.resolve(axios.get('/api/appointments'))])
       .then((all) => {
         setState(prev => ({ ...prev, days: all[0].data, appointments:all[1].data}));
       })
