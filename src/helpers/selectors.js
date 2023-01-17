833229eb8b64595c2e75a0ba27327bec8e830979
KishReboot <russellsalopek@pm.me>
2023-01-17 15:01:42 -0500

    Add an exported function to be a selector

---
+++ src/helpers/selectors.js
@@ -0,0 +1,24 @@
+const matchingAppointments = (appointments, ids) => {
+  
+  const matching = ids.map(id => appointments[id]);
+  
+  return matching;
+
+}
+
+export function getAppointmentsForDay(state, day) {
+  
+  let appointmentArray = [];
+  
+  state.days.map(days => {
+    
+    if (days.name === day) {
+      days.appointments.forEach(appointmentId => appointmentArray.push(appointmentId))
+    }
+
+  })
+  
+  return matchingAppointments(state.appointments, appointmentArray);
+
+};
+
