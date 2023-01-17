8ffc8b05cb9f34f5d4f4a322ad45c9c6c70814e4
KishReboot <russellsalopek@pm.me>
2023-01-17 09:40:00 -0500

    Update props to use .value and .onChange

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,20 +4,19 @@ import DayListItem from "components/DayListItem.js";
 
 export default function DayList(props) {
 
-  const schedule = props.days.map(day => {
-    
+  const days = props.days.map(day => {
     return (
       <DayListItem
-        key={day.id}
-        name={day.name}
-        spots={day.spots}
-        selected={day.name === props.day}
-        setDay={props.setDay}
+        key={day.id} 
+        name={day.name} 
+        spots={day.spots} 
+        selected={day.name === props.value}
+        setDay={props.onChange}
       />
-    )
-  })
+    );
+  });
   
   return (
-    <ul>{schedule}</ul>
+    <ul>{days}</ul>
   )
 }
