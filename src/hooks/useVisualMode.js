16dab169fbae2b02998cf3e9f745db7e479c19bb
KishReboot <russellsalopek@pm.me>
2023-01-18 11:40:32 -0500

    Create new variables for history of what's pushed to mode and being able to go BACK with a function. Add new THIRD constant to test file with tests.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,12 +1,26 @@
 import React, { useState} from "react";
 
 export default function useVisualMode(initial) {
+  
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   function transition(mode) {
 
     setMode(mode);
+    history.push(mode);
   }
 
-  return { mode, transition };
+  function back() { 
+    
+    if (history.length > 1) {
+
+      history.pop();
+      setMode(history[history.length - 1])
+
+    }
+
+  }
+
+  return { mode, transition, back };
 }
