fee30821455bf745534b8b9c58ff2d11ff04c60c
KishReboot <russellsalopek@pm.me>
2023-01-18 11:35:37 -0500

    Add 'SECOND' set of tests, and implement a transition function for setting mode.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,7 +3,10 @@ import React, { useState} from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
-  
+  function transition(mode) {
 
-  return { mode };
+    setMode(mode);
+  }
+
+  return { mode, transition };
 }
