a56d799b00f1aa4de9224d95fd21f2a56b23f547
KishReboot <russellsalopek@pm.me>
2023-01-18 00:23:41 -0500

    Fetch interviewer data use a axios GET request.

--- src/components/Application.js
+++ src/components/Application.js
@@ -19,20 +19,22 @@ export default function Application(props) {
   
   useEffect(() => {
     Promise.all([
-      Promise.resolve(axios.get('/api/days')),
-      Promise.resolve(axios.get('/api/appointments'))])
+     Promise.resolve(axios.get('/api/days')),
+     Promise.resolve(axios.get('/api/appointments')),
+     Promise.resolve(axios.get('/api/interviewers'))
+    ])
       .then((all) => {
-        setState(prev => ({ ...prev, days: all[0].data, appointments:all[1].data}));
+        setState(prev => ({ ...prev, days: all[0].data, appointments:all[1].data, interviewers: all[2].data}));
       })
     }, []);
   
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-  const appointment = dailyAppointments.map(dailyAppointments => {
+  const appointment = dailyAppointments.map(dailyAppointment => {
     
     return (
       <Appointment
-        key={dailyAppointments.id}
-        {...dailyAppointments}
+        key={dailyAppointment.id}
+        {...dailyAppointment}
       />
     )
   });
